26a37646e9efbc0fb0555f3ea7ac043f168500d8
Kinnon McAlister <kinnonmcalister@gmail.com>
2018-04-05 18:09:33 -0700

    Refactored script
    Simplified code
    Better conforms to coding standards

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-function genrow(a) {
-  name = a['album'];
-  edited = a['edited'];
-  deleted = a['deleted'];
-  total = a['total']
-  return "<tr>\n" +
-         "  <td class=\"name-row\">" + name + "</td>\n" +
-         "  <td class=\"prog-row\">" + edited + "/" + deleted + "/" + total + "</td>\n" +
-         "  <td class=\"bar-row\">\n" +
-         "    <progress class=\"edited\" max=\"" + total +
-         "\" value=\"" + edited + "\"></progress>\n" +
-         "    <progress class=\"deleted\" max=\"" + total +
-         "\" value=\"" + deleted + "\"></progress>\n" +
-         "  </td>" +
-         "</tr>\n";
+function genRow(a) {
+  'use strict';
+  let name = a.album,
+      edited = a.edited,
+      deleted = a.deleted,
+      total = a.total;
+  return $('<tr\>').append(
+    $('<td\>', {class: 'name-row', text: name}),
+    $('<td\>', {class: 'prog-row', text: [edited, deleted, total].join('/')}),
+    $('<td\>', {class: 'bar-row'}).append(
+      $('<progress\>', {class: 'edited', max: total, value: edited}),
+      $('<progress\>', {class: 'deleted', max: total, value: deleted})
+    )
+  );
 }
 
 $(document).ready(function() {
-  var ip = $("#table_ip");
-  var complete = $("#table_complete");
-  var totaledited = $("#totaledited");
-  var totaldeleted = $("#totaldeleted");
-  var label = $("#label");
-  var edited = 0;
-  var deleted = 0;
-  var total = 0;
-  for(i=0; i < data.length; i++) {
-    for(j=0; j < data[i]['albums'].length; j++) {
-      album = data[i]['albums'][j];
-      if(album['edited'] == album['total'] - album['deleted']) {
-        complete.append(genrow(album));
+  'use strict';
+  let ip = $('#table_ip'),
+      complete = $('#table_complete'),
+      totaledited = $('#totaledited'),
+      totaldeleted = $('#totaldeleted'),
+      label = $('#label'),
+      edited = 0,
+      deleted = 0,
+      total = 0;
+  for(let i=0; i < data.length; i++) {
+    for(let j=0; j < data[i].albums.length; j++) {
+      let album = data[i].albums[j];
+      if(album.edited == album.total - album.deleted) {
+        complete.append(genRow(album));
       } else {
-        ip.append(genrow(album));
+        ip.append(genRow(album));
       }
-      edited += album['edited'];
-      deleted += album['deleted'];
-      total += album['total'];
+      edited += album.edited;
+      deleted += album.deleted;
+      total += album.total;
     }
   }
 
@@ -42,5 +42,5 @@ $(document).ready(function() {
   totaledited.attr('value', edited);
   totaldeleted.attr('max', total);
   totaldeleted.attr('value', deleted);
-  label.text(edited + '/' + deleted + '/' + total);
+  label.text([edited, deleted, total].join('/'));
 });
